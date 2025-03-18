8946de258e114749b6a9aca251530024526b3a33
Abraham Vijai <abrahamvijai03@gmail.com>
2025-03-18 13:04:08 -0400

    Created project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img
+        src={reactLogo}
+        alt="React logo"
+        style={{ flex: 1,width: 300, height: 300 }}
+      />
+    </div>
+  );
+}
+
+export default App;
