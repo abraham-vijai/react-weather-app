0e62e14e3d318676ea93a7dcd0c50d39f883c0d6
Abraham Vijai <abrahamvijai03@gmail.com>
2025-03-23 14:57:16 -0400

    Added popup image for pfp

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -18,15 +18,28 @@ import {
 import SettingsOption from "./components/SettingsOption";
 import IOSSwitch from "./components/IOSSwitch";
 import TextIcon from "./components/TextIcon";
+import { useState } from "react";
+import TouchableOpacity from "./components/TouchableOpacity";
+import { Dialog } from "@mui/material";
 
 const Settings = () => {
+  const [isImageVisible, setImageVisibility] = useState(false);
   return (
     <div style={mainDivStyle}>
       <div style={profileDivStyle}>
         {/* Profile circle */}
-        <div style={profileCircleStyle}>
-          <span style={{ color: "white", fontSize: "1.5rem" }}>Profile</span>
-        </div>
+        <TouchableOpacity
+          style={{ border: 0, padding: 0, margin: 0, borderRadius: "50%" }}
+          onClick={() => setImageVisibility(true)}
+        >
+          <div style={profileCircleStyle}>
+            <span style={{ color: "white", fontSize: "1.5rem" }}>Profile</span>
+          </div>
+        </TouchableOpacity>
+        {/* Enlarged Image */}
+        <Dialog open={isImageVisible} onClose={() => setImageVisibility(false)}>
+          <img src="https://picsum.photos/200" alt="no image" />
+        </Dialog>
         {/* User name */}
         <span style={{ ...locationIconStyle, fontSize: 30 }}>
           John Smith
